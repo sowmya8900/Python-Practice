d507d8c86732264f43f88f58dcf620cc8cf74231
Sowmya Yellapragada <56383804+sowmya8900@users.noreply.github.com>
2020-03-23 19:56:34 +0530

    Add files via upload

---
+++ Numbers.py
@@ -0,0 +1,6 @@
+radius= float(input("Enter the radius of the circle"))
+import math
+area= float(math.pi*(radius**2))
+circumference= float(2*math.pi*radius)
+print("Area=", round(area, 3))
+print("circumference=", round(circumference,3))
---
+++ strings.py
@@ -0,0 +1,8 @@
+f_name= str(input("first name"))
+m_name= str(input("middle name"))
+l_name= str(input("last name"))
+print("Your initials are", f_name[0]+m_name[0]+l_name[0])
+number= "037-00901-00027"
+print("Country code:", number[0:3])
+print("Product code:", number[4:9])
+print("Batch number:", number[10:])
