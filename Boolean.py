0f79485d4830f319599bb594719164d55a126aa4
Sowmya Yellapragada <56383804+sowmya8900@users.noreply.github.com>
2020-03-28 21:20:06 +0530

    Add files via upload

---
+++ Boolean.py
@@ -0,0 +1,8 @@
+my_age=19
+your_age=int(input("What's your age?"))
+if(my_age>your_age):
+    print("You are younger than me!")
+elif(my_age<your_age):
+    print("You are elder than me!")
+else:
+    print("We both are of same age!")
